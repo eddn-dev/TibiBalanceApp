0352a0c1ee40e0eb415dcc71ecf8e77be6e7b2ef
MiguelAG <miguelredmi@gmail.com>
2025-05-01 21:01:34 -0600

    Contenedor de perfil

---
+++ app/src/main/java/com/app/tibibalance/ui/components/ProfileContainer.kt
@@ -0,0 +1,52 @@
+package com.app.tibibalance.ui.components
+
+import com.app.tibibalance.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ProfileContainer(
+    imageResId: Int,
+    size: Dp = 96.dp, // tamaño del círculo
+    backgroundColor: Color = Color.LightGray,
+    contentDescription: String? = null,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .clip(CircleShape)
+            .background(backgroundColor),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = imageResId),
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun ProfilePreview() {
+    ProfileContainer(
+        imageResId = R.drawable.imagenprueba,
+        size = 80.dp,
+        contentDescription = "Foto de perfil"
+    )
+}
