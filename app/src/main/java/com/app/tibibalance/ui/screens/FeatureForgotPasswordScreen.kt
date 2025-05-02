33cfa7acbd4717333b5d4068de9a82efa11a06ee
alejandroHZ <204880425+AlejandroHZ858@users.noreply.github.com>
2025-05-02 09:01:59 -0600

    patallas antes del pull

--- app/src/main/java/com/app/tibibalance/ui/screens/FeatureForgotPasswordScreen.kt
+++ app/src/main/java/com/app/tibibalance/ui/screens/FeatureForgotPasswordScreen.kt
@@ -1,23 +1,15 @@
 package com.app.tibibalance.ui.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.tooling.preview.Preview
 
 import com.app.tibibalance.R
 import com.app.tibibalance.ui.components.*
--- app/src/main/java/com/app/tibibalance/ui/screens/FeatureResetPasswordScreen.kt
+++ app/src/main/java/com/app/tibibalance/ui/screens/FeatureResetPasswordScreen.kt
@@ -1,15 +1,12 @@
 package com.app.tibibalance.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,7 +24,6 @@ import com.app.tibibalance.ui.components.Header
 import com.app.tibibalance.ui.components.ImageContainer
 import com.app.tibibalance.ui.components.InputPassword
 import com.app.tibibalance.ui.components.PrimaryButton
-import com.app.tibibalance.ui.components.TextButtonLink
 
 @Composable
 fun FeatureResetPasswordScreen() {
