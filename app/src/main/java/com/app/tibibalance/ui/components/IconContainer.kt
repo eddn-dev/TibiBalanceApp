f3ac966383bc229ce399a3eb818de6cdcb03ca48
alejandroHZ <204880425+AlejandroHZ858@users.noreply.github.com>
2025-05-02 02:41:32 -0600

    pantallas terminadas Alejandro Login,ResetPassword y avance AddHabit

--- app/src/main/java/com/app/tibibalance/ui/components/IconContainer.kt
+++ app/src/main/java/com/app/tibibalance/ui/components/IconContainer.kt
@@ -1,6 +1,5 @@
 package com.app.tibibalance.ui.components
 
-import com.app.tibibalance.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -10,7 +9,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Suppress("UNUSED_FUNCTION")
---
+++ app/src/main/java/com/app/tibibalance/ui/screens/FeatureAddHabitScreen.kt
@@ -0,0 +1,192 @@
+package com.app.tibibalance.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.app.tibibalance.R
+import com.app.tibibalance.ui.components.Description
+import com.app.tibibalance.ui.components.FormContainer
+import com.app.tibibalance.ui.components.Header
+import com.app.tibibalance.ui.components.ImageContainer
+import com.app.tibibalance.ui.components.InputSelect
+import com.app.tibibalance.ui.components.InputText
+import com.app.tibibalance.ui.components.SecondaryButton
+import com.app.tibibalance.ui.components.Subtitle
+
+
+@Composable
+fun FeatureAddHabitScreen() {
+    // Contenido de la pantalla
+    GradientBackgroundScreen {
+        // Contenido de la pantalla
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+            //.background(Color(0xFFf5f5f5)) // Fondo gris claro
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(16.dp)
+            ) {
+                Header(
+                    title = "Agregar Hábito",
+                    showBackButton = false,
+                    profileImage = null
+                )
+
+                FormContainer (
+                    modifier = Modifier
+                        .height(734.dp)  // Establece un alto específico
+                ){
+
+                    Subtitle(
+                        text = "Crear mi propio hábito",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(Alignment.CenterVertically),
+                        textAlign = TextAlign.Center
+                    )
+
+                    Description(
+                        text = "Nombre: *",
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Start
+                    )
+                    var nombre by remember { mutableStateOf("Nora Soto") }
+                    InputText(
+                        value = nombre,
+                        onValueChange = { nombre = it }
+                    )
+                    Spacer(modifier = Modifier.height(7.dp))
+
+                    Description(
+                        text = "Descripción:",
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Start
+                    )
+                    var descripcion by remember { mutableStateOf("Nora Soto") }
+                    InputText(
+                        value = descripcion,
+                        onValueChange = { descripcion = it }
+                    )
+                    Spacer(modifier = Modifier.height(7.dp))
+
+                    Description(
+                        text = "frecuencia: *",
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Start
+                    )
+                    var frecuencia by remember { mutableStateOf("Diario") }
+                    InputSelect(
+                        options = listOf("Diario", "Semanal", "Mensual"),
+                        selectedOption = frecuencia,
+                        onOptionSelected = { frecuencia = it }
+                    )
+                    Spacer(modifier = Modifier.height(7.dp))
+
+                    Description(
+                        text = "Duración:",
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Start
+                    )
+                    var duracion by remember { mutableStateOf("Nora Soto") }
+                    InputText(
+                        value = duracion,
+                        onValueChange = { duracion = it }
+                    )
+                    Spacer(modifier = Modifier.height(7.dp))
+
+
+                    Description(
+                        text = "Categoría: *",
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Start
+                    )
+                    var categoria by remember { mutableStateOf("Diario") }
+                    InputSelect(
+                        options = listOf("Diario", "Semanal", "Mensual"),
+                        selectedOption = categoria,
+                        onOptionSelected = { categoria = it }
+                    )
+
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 16.dp),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Description(
+                            text = "Notificación: *",
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            textAlign = TextAlign.Start
+                        )
+
+                        /*ImageContainer(
+                            imageResId = R.drawable.ic_buttonnotification_image,  // Reemplaza con tu imagen
+                            contentDescription = "Imagen de inicio", // Descripción de la imagen
+                            modifier = Modifier
+                                //.fillMaxWidth()
+                                .height(70.dp)
+                                .width(70.dp)
+                                .padding(bottom = 32.dp)
+                        )*/
+                    }
+
+
+                    Spacer(modifier = Modifier.height(40.dp))
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 16.dp),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        Spacer(modifier = Modifier.width(8.dp))
+                        SecondaryButton(
+                            text = "Guardar",
+                            onClick = {}
+                        )
+                        Spacer(modifier = Modifier.width(20.dp)) // 1f es un peso relativo
+
+
+                        SecondaryButton(
+                            text = "Cancelar",
+                            onClick = {}
+                        )
+                    }
+
+                }
+
+            }
+        }
+    }
+}
